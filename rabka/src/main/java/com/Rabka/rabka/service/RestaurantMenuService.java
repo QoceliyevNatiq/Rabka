27b704092816b44fae014c35c836703387f93ddf
QoceliyevNatiq <qoceliyevnatiq@gmail.com>
2026-06-26 22:45:53 +0400

    some restaurantMenu service fixes

--- rabka/src/main/java/com/Rabka/rabka/service/RestaurantMenuService.java
+++ rabka/src/main/java/com/Rabka/rabka/service/RestaurantMenuService.java
@@ -1,9 +1,19 @@
 package com.Rabka.rabka.service;
 
 
+import com.Rabka.rabka.dto.RestaurantMenu.RestaurantMenuCreateDto;
+import com.Rabka.rabka.dto.RestaurantMenu.RestaurantMenuResponseDto;
+import com.Rabka.rabka.dto.RestaurantMenu.RestaurantMenuUpdateDto;
+import com.Rabka.rabka.entity.RestaurantMenu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface RestaurantMenuService {
-    
+    void deleteById(Long menuId);
+    RestaurantMenuResponseDto create(RestaurantMenuCreateDto dto);
+    RestaurantMenuResponseDto update(RestaurantMenuUpdateDto dto);
+    RestaurantMenuResponseDto findById(Long id);
+    Page<RestaurantMenuResponseDto> findAll(Pageable pageable);
 }
--- rabka/src/main/java/com/Rabka/rabka/service/impl/RestaurantMenuServiceImpl.java
+++ rabka/src/main/java/com/Rabka/rabka/service/impl/RestaurantMenuServiceImpl.java
@@ -1,4 +1,68 @@
 package com.Rabka.rabka.service.impl;
 
-public class RestaurantMenuServiceImpl {
+import com.Rabka.rabka.dto.RestaurantMenu.RestaurantMenuCreateDto;
+import com.Rabka.rabka.dto.RestaurantMenu.RestaurantMenuResponseDto;
+import com.Rabka.rabka.dto.RestaurantMenu.RestaurantMenuUpdateDto;
+import com.Rabka.rabka.entity.RestaurantMenu;
+import com.Rabka.rabka.exception.ResourceNotFoundException;
+import com.Rabka.rabka.repo.RestaurantMenuRepository;
+import com.Rabka.rabka.service.RestaurantMenuService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class RestaurantMenuServiceImpl implements RestaurantMenuService {
+
+    private final RestaurantMenuRepository restaurantMenuRepository;
+    private final
+
+    @Override
+    public void deleteById(Long menuId) {
+        log.debug("Delete RestaurantMenu start | id: {}", menuId);
+        if(!restaurantMenuRepository.existsById(menuId)){
+            log.warn("Delete RestaurantMenu | id: {}", menuId);
+            throw new ResourceNotFoundException("deleteById","menuId",menuId);
+        }
+        restaurantMenuRepository.deleteById(menuId);
+        log.info("Delete RestaurantMenu ended | id: {}", menuId);
+    }
+
+    @Override
+    public RestaurantMenuResponseDto create(RestaurantMenuCreateDto dto) {
+//        log.debug("RestaurantMenu create start");
+
+    }
+
+    @Override
+    @Transactional
+    public RestaurantMenuResponseDto update(RestaurantMenuUpdateDto dto) {
+        log.debug("RestaurantMenu update start | id: {}", dto.id());
+        RestaurantMenu restaurantMenu = restaurantMenuRepository.findById(dto.id())
+                .orElseThrow(() ->{
+                    log.warn("RestaurantMenu update | id: {}", dto.id());
+                  return  new ResourceNotFoundException("RestaurantMenu", "id", dto.id());
+                });
+        if(dto.name() != null){
+            restaurantMenu.setName(dto.name());
+        }
+        restaurantMenu.setName(dto.name());
+        log.info("RestaurantMenu update ended | id: {}", dto.id());
+        return mapper
+    }
+
+    @Override
+    public RestaurantMenuResponseDto findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Page<RestaurantMenuResponseDto> findAll(Pageable pageable) {
+        return null;
+    }
 }
